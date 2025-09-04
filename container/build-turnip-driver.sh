6b107b9a6cfd4923a001ad2239a55259cf9067f4
Shiumano <75152752+shiumano@users.noreply.github.com>
2025-09-04 14:21:01 +0900

    Allow implicit function declarations
    Minor edits are definitely faster to do locally.

--- container/build-turnip-driver.sh
+++ container/build-turnip-driver.sh
@@ -2,6 +2,8 @@
 set -x
 set -e
 
+export CFLAGS="-Wno-implicit-function-declaration"
+
 source() {
     command -v git &> /dev/null || apt-get install git ca-certificates -y --no-install-suggests --no-install-recommends
     [ ! -d "mesa" ] && git clone https://gitlab.freedesktop.org/zsnow/mesa --depth=1
